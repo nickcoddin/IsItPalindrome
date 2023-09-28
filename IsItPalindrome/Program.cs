023ec24f22afd6b7ad27d50faa85a6f9e7682665
Nick <nikoloz.de@gmail.com>
2023-09-28 16:47:40 +0400

    Add project files.

---
+++ IsItPalindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace IsPalindrome
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string textOne = "Atami itesa aseTi imata";
+            string textTwo = "Nikoloz demetrashvili";
+
+            Console.WriteLine(IsPalindrome(textOne));
+            Console.WriteLine(IsPalindrome(textTwo));
+        }
+
+        public static bool IsPalindrome(string text)
+        {
+            char[] charArray = new char[text.Count()];
+            int increment = 0;
+
+            foreach (char letter in text.ToUpper())
+            {
+                if (char.IsLetterOrDigit(letter))
+                {
+                    Array.Resize(ref charArray, increment + 1);
+                    charArray[increment] = letter;
+                    increment++;
+                }
+            }
+            return charArray.SequenceEqual(charArray.Reverse());
+        }
+    }
+}
